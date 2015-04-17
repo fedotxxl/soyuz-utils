e34ffb21675ac06ae283ad6f117933d9d27662ae
fbelov <fedor.belov@mail.ru>
2015-04-18 00:32:43 +0300

    classPathFile

--- src/main/java/io/belov/soyuz/utils/ClassPathFile.java
+++ src/main/java/io/belov/soyuz/utils/ClassPathFile.java
@@ -7,9 +7,11 @@ import java.io.InputStream;
  */
 public class ClassPathFile {
 
+    private static final Object o = new Object();
+
     //http://stackoverflow.com/questions/1464291/how-to-really-read-text-file-from-classpath-in-java
-    InputStream asStream(String filePath) {
-        return this.getClass().getClassLoader().getResourceAsStream(filePath);
+    public static InputStream asStream(String filePath) {
+        return o.getClass().getResourceAsStream(filePath.startsWith("/") ? filePath : "/" + filePath);
     }
 
 }
